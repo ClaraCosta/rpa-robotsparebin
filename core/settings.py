ecc1601bb52baccf8941b6a0165d940f58a6ae9b
Clara <ClaraCosta@users.noreply.github.com>
2024-11-29 09:05:22 -0300

    Inicializando repositório e definindo variáveis

---
+++ core/settings.py
@@ -0,0 +1,8 @@
+# core/settings.py
+BASE_URL = "https://robotsparebinindustries.com/"
+USERNAME = "maria"
+PASSWORD = "thoushallnotpass"
+
+EXCEL_FILE_PATH = "resources/SalesData.xlsx"
+PDF_OUTPUT_PATH = "output/sales_results.pdf"
+SCREENSHOT_PATH = "output/sales_summary.png"
